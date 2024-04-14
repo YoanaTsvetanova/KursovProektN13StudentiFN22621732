282bf4cba753774d34374381d16c3af8b3bf3b38
yoana <yoana.tsvetanova06@gmail.com>
2024-04-14 18:56:48 +0300

    "Initial commit: Added IntelliJ project files and basic Java classes:
    Added .idea/vcs.xml
    Added src/bg/tu_varna/sit/b1/f22621732/Specialty.java
    Modified src/bg/tu_varna/sit/b1/f22621732/Student.java
    Added src/bg/tu_varna/sit/b1/f22621732/Subject.java"

--- src/bg/tu_varna/sit/b1/f22621732/Main.java
+++ src/bg/tu_varna/sit/b1/f22621732/Main.java
@@ -1,5 +1,13 @@
 package bg.tu_varna.sit.b1.f22621732;
 
 public class Main {
+    public static void main(String[] args) {
+//        Student student1=new Student("yoni","22626","sit","3","active");
+//
+//        student1.ToString();
+//
+//        student1.setName("aksi");
+//        student1.ToString();
+    }
 
 }
--- src/bg/tu_varna/sit/b1/f22621732/Student.java
+++ src/bg/tu_varna/sit/b1/f22621732/Student.java
@@ -1,4 +1,5 @@
 package bg.tu_varna.sit.b1.f22621732;
+import java.util.*;
 
 public class Student {
 
@@ -9,6 +10,8 @@ public class Student {
     private String group;
     private String status;
     private double averageGrade;
+    private Map<String, String> enrolledCourses;
+    private Map<String, Integer> grades;
 
     public Student(String name, String facultyNumber, String specialty, String group, String status) {
         this.name = name;
@@ -17,33 +20,78 @@ public class Student {
         this.group = group;
         this.status = status;
         this.averageGrade = 0.0;
+        this.grades = new HashMap<>();
+        this.enrolledCourses = new HashMap<>();
     }
 
     public String getName() {
+
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getFacultyNumber() {
+
         return facultyNumber;
     }
 
+    public void setFacultyNumber(String facultyNumber) {
+        this.facultyNumber = facultyNumber;
+    }
+
     public int getCurrentCourse() {
+
         return currentCourse;
     }
 
+    public void setCurrentCourse(int currentCourse) {
+        this.currentCourse = currentCourse;
+    }
+
     public String getSpecialty() {
+
         return specialty;
     }
 
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
     public String getGroup() {
+
         return group;
     }
 
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
     public String getStatus() {
+
         return status;
     }
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public double getAverageGrade() {
+
         return averageGrade;
     }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    /*public void ToString(){
+        System.out.println("name:"+this.name);
+        System.out.println("group:"+this.group);
+        System.out.println("status:"+this.status);
+
+
+    }*/
 }
